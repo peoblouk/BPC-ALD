5245fb7a2cf7de8131f73641c51cd161c42097a0
peoblouk <ahojcz48@gmail.com>
2024-04-19 17:29:38 +0200

    upload re

--- ALD TESTY/2_termin/Test 2f_(ADT_množina_SET_set_equal) - 2023/TSet_set_equal/TSet_sorted_dynamic_array/testplan_equal3.h
+++
@@ -1,8 +0,0 @@
-{
-TEST_MESSAGE("Test rovnosti mnozin")
-TEST_OPERATION(set_insert)
-TEST_INPUTS("ahoj", "nazdar", "cau", "alfa", "beta")
-TEST_OUTPUT(1, 1, 1, 1, 1)
-//TEST_REPORT_AFTER
-//TEST_PRINT_MEMORY_STATS
-},
